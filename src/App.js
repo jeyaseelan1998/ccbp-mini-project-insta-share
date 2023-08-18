8f8cdf68e6f4349452cbff21fef28090fa795eed
jeyaseelan windows 10 <jeyaseelan.work@gmail.com>
2023-08-18 11:44:02 +0530

    login, Header and Not found componets added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import ProtectedRoute from './components/ProtectedRoute'
+import Header from './components/Header'
+import Login from './components/Login'
+import NotFound from './components/NotFound'
+
 import './App.css'
 
-const App = () => <div>Hello World</div>
+const App = () => (
+  <>
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/" component={Header} />
+      <ProtectedRoute exact path="/my-profile" component={Header} />
+      <Route exact path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
+    </Switch>
+  </>
+)
 
 export default App
